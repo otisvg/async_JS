3bede170dcb70c6c2e602bd9bf06e093220a9b2c
Otis <otisvickersgraver@gmail.com>
2020-11-02 12:37:41 +0000

    code snippet 1. assessed

---
+++ async.js
@@ -0,0 +1,18 @@
+// For each piece of code below:
+
+// Read the code.
+//
+// Write down your prediction of what would happen if you run it (what will appear on the console, and in what order).
+//
+// Run the code by pasting it in the console of your browser.
+//
+// Compare the actual result with your predictions.
+//
+// Using previous knowledge, given resources and the internet explain what actually happened.
+
+//1.
+//Prediction: hello will be printed into the console after the specified delay (milliseconds?)
+setTimeout(function() {
+  console.log('hello');
+}, 1000);
+//What actually happened: The setTimeout() method invoked my function after a delay of 1000 milliseconds (1 second).
